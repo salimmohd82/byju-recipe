be411327cc4809ab2fdc851839dc29b0da0ae297
Salim, Mohammed <Mohammed_Salim@intuit.com>
2019-05-07 11:21:39 +0530

    Updates

---
+++ precache-manifest.d9f9b5ef72137d5db93de972d66c5a24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf6831c675152b544f7126736df3f2",
+    "url": "/byju-recipe/index.html"
+  },
+  {
+    "revision": "01b56bc5115466a8e35b",
+    "url": "/byju-recipe/static/css/main.9285d2db.chunk.css"
+  },
+  {
+    "revision": "3fd529b43ffab1b2de88",
+    "url": "/byju-recipe/static/js/2.1a112f09.chunk.js"
+  },
+  {
+    "revision": "01b56bc5115466a8e35b",
+    "url": "/byju-recipe/static/js/main.370504f4.chunk.js"
+  },
+  {
+    "revision": "a1d3fb97fda56ce0c47a",
+    "url": "/byju-recipe/static/js/runtime~main.992a33da.js"
+  }
+]);
